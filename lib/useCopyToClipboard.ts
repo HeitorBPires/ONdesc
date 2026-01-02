985159f4ceeb76b68a02806016d39a583ba23e22
Heitor <torbeckpires@gmail.com>
2026-01-02 17:46:06 -0300

    Ajuste no useCopyToClipboard

--- lib/useCopyToClipboard.ts
+++ lib/useCopyToClipboard.ts
@@ -3,8 +3,14 @@ import { useState } from "react";
 export function useCopyToClipboard(timeout = 1500) {
   const [copied, setCopied] = useState(false);
 
+  function extractNumber(text: string) {
+    return text.replace(/[^\d,.-]/g, "");
+  }
+
   async function copy(text: string) {
-    await navigator.clipboard.writeText(text);
+    const onlyNumbers = extractNumber(text);
+
+    await navigator.clipboard.writeText(onlyNumbers);
     setCopied(true);
 
     setTimeout(() => setCopied(false), timeout);
