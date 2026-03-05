ae54e71f583d23b5a602f01bb3fa38693212b7db
Heitor <torbeckpires@gmail.com>
2026-03-05 18:29:36 -0300

    add logo login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,17 @@
 import { Suspense } from "react";
 import LoginForm from "@/components/auth/LoginForm";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-gray-50 via-white to-blue-50 px-4">
+    <main className="flex flex-col gap-10 min-h-screen items-center justify-center bg-linear-to-br from-gray-50 via-white to-blue-50 px-4 ">
+      <Image
+        className="w-[150px]"
+        src="/img/logoon.svg"
+        alt="Logo"
+        width={200}
+        height={100}
+      />
       <Suspense fallback={null}>
         <LoginForm />
       </Suspense>
